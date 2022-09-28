cd328d67375529ccc1d71a373a85e3120f1e8c4b
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-28 23:36:09 +0600

    try to fix responsive

--- src/components/Activitys/Activitys.js
+++ src/components/Activitys/Activitys.js
@@ -17,7 +17,7 @@ const Activitys = () => {
               The Daily Activity
             </h1>
             <p className="text-lg font-bold py-2">Start Your Morning</p>
-            <div className='grid grid-cols-3 gap-4 my-8'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-8'>
               {activitys.map((activity) => (
                 <ActivityItem
                   activity={activity}
